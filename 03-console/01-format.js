9e1e3a03e2d5a3e1aab772ac682fc2fa1231ee35
gaoruixue1215 <1171331232@qq.com>
2019-09-10 12:10:09 +0800

    add

---
+++ 03-console/01-format.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const log = console.log;
+
+var wd = {
+    'Name':'郜瑞雪',
+    'QQ':'1171331232',
+    'Age':20
+
+};
+
+log('Name:%s\t Age:%d',wd.Name,wd.Age);
+log('gaoruixue Info:',wd);
+
+log('Name:',wd.Name);
+log('Age is ${wd.Age}');
+
